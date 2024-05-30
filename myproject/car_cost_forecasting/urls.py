489e6f5d99c739eeb5b6d799679945d278abc033
Nezhana <104068404+Nezhana@users.noreply.github.com>
2024-05-30 20:31:29 +0300

    Update urls.py

--- myproject/car_cost_forecasting/urls.py
+++ myproject/car_cost_forecasting/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import car_cost_view
 
 urlpatterns = [
-    path('car-cost-forecasting/', car_cost_view, name='car-cost'),
+    path('', car_cost_view, name='car-cost'),
 ]
