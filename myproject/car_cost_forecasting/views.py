b03c208afc2e6575b086f787953ce0d6978ef21b
Nezhana <104068404+Nezhana@users.noreply.github.com>
2024-05-30 20:31:14 +0300

    Update views.py
    View done (?)

--- myproject/car_cost_forecasting/views.py
+++ myproject/car_cost_forecasting/views.py
@@ -1,4 +1,24 @@
+from .index import CarPricePredictor
+from .forms import ForecastForm
 from django.shortcuts import render
 
 def car_cost_view(request):
-    return render(request, 'forecast.html')
+    if request.method == 'POST':
+        form = ForecastForm(request.POST)
+        print(request.POST['latlaunch'], type(request.POST['latlaunch']))
+        predicted_price = forecasting(request.POST)
+        result = f'${str(predicted_price * 1000)[:2]},{str(predicted_price * 1000)[2:5]}'
+        print(predicted_price, '=', result)
+    else:
+        result = ''
+        form = ForecastForm()
+
+    data = {'form': form, 'result': result}
+
+    return render(request, "forecast.html", data)
+    # return render(request, 'forecast.html')
+
+def forecasting(data):
+    predictor = CarPricePredictor()
+    predicted_price = predictor.predict_price(data['brand'], data['model'], data['esize'], data['power'], data['latlaunch'])
+    return predicted_price
